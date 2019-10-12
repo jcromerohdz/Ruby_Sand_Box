b846bddd550310f0067f4a6b2eef35f6217a115e
Geek Mode <jcromerohdz@gmail.com>
2019-10-12 10:10:05 -0700

    Adding bubble_sort

---
+++ Session_4/chapter_1/chapter_1.rb
@@ -0,0 +1,33 @@
+5.times { puts "Odelay!" }
+
+exit unless "restaurant".include? "aura"
+
+['toast', 'cheese', 'wine'].each { |food| puts food.capitalize  }
+
+# Variables
+teddy_bear_fee = 121.08
+puts teddy_bear_fee
+# total = orphan_fee + teddy_bear_fee + gratuity
+
+# Numbers
+population = 12
+puts population.to_s
+
+# Strings
+avril_quote = "I'm a lot wiser.  Now I know
+what the business is like -- what you have
+to do and how to work it."
+
+puts avril_quote
+
+# Constant
+EmpireStateBuilding = "350 5th Avenue, NYC, NY"
+puts EmpireStateBuilding
+
+
+2.times {puts "bye, dude!"}
+
+print ('a'..'z')
+
+print "Type and be diabolical: "
+idea_backwards = gets.reverse
---
+++ complete_ruby_programmer/Data_structures/bubble_sort.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+def bubble_sort(arr)
+  array_length = arr.length
+  return arr if array_length < 2
+
+  max_index = array_length - 2
+  swapped_elements = true
+
+  while swapped_elements
+    swapped_elements = false
+
+    (0..max_index).each do |i|
+      element = arr[i]
+      next_element = arr[i + 1]
+      next unless element > next_element
+
+      arr[i] = next_element
+      arr[i + 1] = element
+      swapped_elements = true
+    end
+  end
+
+  arr
+end
+
+def bubble_sort_by(arr)
+  if block_given?
+    swap = -1
+    while swap != 0
+      swap = 0
+      (0..(arr.length - 2)).each do |i|
+        if yield(arr[i], arr[i + 1]).positive?
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+          swap += 1
+        end
+      end
+    end
+  else
+    arr.sort unless  arr
+    raise "Block is needed!"
+  end
+
+  puts '--------------'
+  if arr.class.to_s == "Array"
+    p arr.class
+    output = arr.join(', ')  
+    puts "[#{output}]"
+  else
+    raise "No block is given"
+  end
+  
+end
+
+p bubble_sort([4, 3, 78, 2, 0, 2])
+u = "som"
+bubble_sort_by(%w[hey hello hi]) { |left, right| left.length - right.length }
+
