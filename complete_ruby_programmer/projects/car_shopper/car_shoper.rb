684269f52b0d4a4042b06ea97e094d4d40c54e56
J. Christian Romero <jcromerohdz@gmail.com>
2019-12-25 12:25:14 -0800

    Adding binary tree search and for car_shoper I extract the data into a hash table via nokogiri and httparty

--- complete_ruby_programmer/projects/car_shopper/car_shoper.rb
+++ complete_ruby_programmer/projects/car_shopper/car_shoper.rb
@@ -1,7 +1,28 @@
-require "httparty"
+require 'httparty'
+require 'nokogiri'
+require 'pp'
 
 def car_shoper
+  
+  response = HTTParty.get('https://code.evgenyrahman.com/rubycourse/carlist.html')
+  parse_html = Nokogiri::HTML(response.body)
+
+  car_listing = parse_html.css('.card.car')
+  # pp car_listing.first
+
+  car_listing.each do |each_car|
+    car = {
+      make: each_car.css('.make').text,
+      year: each_car.css('.year').text,
+      price: each_car.css('.price').text,
+      rating: each_car.css('.star.rating').attribute("data-rating").value
+    }
+    pp car
+  end
+
   puts "Welcome to the car shopper!"
+
+  # puts parse_html
 end
 
 car_shoper
