1757420c052433ebbb64f13ab232859d84d0e3a5
J. Christian Romero <jcromerohdz@gmail.com>
2019-12-27 17:46:03 -0800

    Determining Browser

--- complete_ruby_programmer/projects/data_engineering/log_parse.rb
+++ complete_ruby_programmer/projects/data_engineering/log_parse.rb
@@ -1,14 +1,28 @@
-filename = 'data/access-log-20190521-211058.log'
-access_log_lines = File.open(filename, 'r') do |f|
-  f.readlines
+
+def parse_log
+
+  filename = 'data/access-log-20190521-211058.log'
+  access_log_lines = File.open(filename, 'r') do |f|
+    f.readlines
+  end
+
+  signup_log_lines = access_log_lines.select do |line|
+    line.include?('/signup?email=')
+  end
+
+  user_data = signup_log_lines.map do |line|
+    parsed_line = line.split('" "').last
+    determine_browser(parsed_line)
+  end
+
+  puts user_data
 end
 
-signup_log_lines = access_log_lines.select do |line|
-  line.include?('/signup?email=')
+def determine_browser(user_agent)
+  return "Firefox" if user_agent.include?("Firefox") && user_agent.include?("Gecko")
+  return "Chrome" if user_agent.include?("Chrome")
+  return "Safari" if user_agent.include?("Gecko") && user_agent.include?("Safari")
+  "Other"
 end
 
-user_data = signup_log_lines.map do |line|
-  parsed_line = line.split('" "').last
-end
-
-puts user_data
+parse_log
